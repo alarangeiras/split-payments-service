7dab0bfb5df1ac66484486d2f73d37a45dd66fd6
Allan Larangeiras <alarangeiras@gmail.com>
2025-11-07 08:58:33 -0300

    chore: change knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,39 +1,42 @@
 import dotenv from "dotenv";
 import type { Knex } from "knex";
+import config from 'config';
+
+const dbConfig = config.get<{ host: string; port: number; username: string; password: string; database: string }>('database');
 
 if (process.env.NODE_ENV === "dev") dotenv.config();
 
 const defaultConfig: Knex.Config = {
-	client: "pg",
-	connection: {
-		host: process.env.DB_HOST ?? "localhost",
-		port: Number(process.env.DB_PORT ?? 5432),
-		user: process.env.DB_USER,
-		database: process.env.DB_NAME,
-		password: process.env.DB_PASSWORD,
-	},
-	migrations: {
-		tableName: "knex_migrations",
-	},
+  client: "pg",
+  connection: {
+    host: dbConfig.host,
+    port: dbConfig.port,
+    user: dbConfig.username,
+    database: dbConfig.database,
+    password: dbConfig.password
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
 };
 
-const config: { [key: string]: Knex.Config } = {
-	dev: defaultConfig,
+const knexConfig: { [key: string]: Knex.Config } = {
+  dev: defaultConfig,
 
-	test: {
-		...defaultConfig,
-		connection: {
-			host: "localhost",
-			port: 5433,
-			user: "split_service_user",
-			database: "split-service",
-			password: "split_service_pass",
-		},
-	},
+  test: {
+    ...defaultConfig,
+    connection: {
+      host: "localhost",
+      port: 5433,
+      user: "split_service_user",
+      database: "split-service",
+      password: "split_service_pass",
+    },
+  },
 
-	staging: defaultConfig,
+  staging: defaultConfig,
 
-	production: defaultConfig,
+  production: defaultConfig,
 };
 
-module.exports = config;
+module.exports = knexConfig;
